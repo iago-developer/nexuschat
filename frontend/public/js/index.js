096ea46725de063e85d939cd766e7885a82b4b28
Matheus Iago dos Santos Silva <162377902+iago-developer@users.noreply.github.com>
2024-09-29 16:53:04 -0300

    update change server url

--- frontend/public/js/index.js
+++ frontend/public/js/index.js
@@ -79,7 +79,7 @@ const handleLogin = event => {
     login.style.display = "none";
     chat.style.display = "flex";
 
-    websocket = new WebSocket("ws://localhost:8080");
+    websocket = new WebSocket("wss://nexuschat-6cy5.onrender.com");
     websocket.onmessage = processMessage;
 };
 
